3b62146bd019aa52a34fdb58b8d5fde3ce51e0ce
Luisa Blom <blomluisa@gmail.com>
2021-09-08 10:35:44 +0200

    page book fixed and styled

--- book.js
+++ book.js
@@ -1,10 +1,10 @@
 let images = [
-    "img/book/front-page-adventure-in-peru.jpg",
-    "img/book/book1.jpg",  
-    "img/book/a-out-007.jpg",
-    "img/book/b-out-000.jpg",
-    "img/book/book3.jpg",
-    "img/book/b-out-002.jpg",
+"img/portfolio_2021/Matura/front-page.jpg",
+"img/portfolio_2021/Matura/eating.jpg",
+"img/portfolio_2021/Matura/aua.jpg",
+"img/portfolio_2021/Matura/fire.jpg",
+"img/portfolio_2021/Matura/fly.jpg",
+"img/portfolio_2021/Matura/traktor.jpg",
 ];
 
 let currentImage = 0;
@@ -15,7 +15,7 @@ function showImg(difference) {
     newImageIndex = (images.length + newImageIndex) % images.length;
     console.log(newImageIndex);
     currentImage = newImageIndex;
-    $(".frontimage").attr("src", images[currentImage]);
+    $(".flip-book").attr("src", images[currentImage]);
 }
 
 $(".arrow-left").click(function() {
