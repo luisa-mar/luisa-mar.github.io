a53469475c51d81d949084a339e15c7b66087a3f
Luisa Blom <blomluisa@gmail.com>
2021-09-09 22:23:18 +0200

    deleting all access images which i do not use.
    I moved everything to website 1

--- products.js
+++
@@ -1,7 +0,0 @@
-$(".open-on-hover").hover(function(event){
-$(".card").show();
-var imgtag = $(event.target).closest('.card').find('img');
-    var imgurl = imgtag.attr('src');
-    var img = $('<img>', {'src': imgurl});
-    card.append(img);
-})
